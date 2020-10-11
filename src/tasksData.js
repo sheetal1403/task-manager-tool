5b2e616695f7285e8895367d837ecd6d24235bc3
Sheetal R <sheetalraghupathi14@gmail.com>
2020-10-11 10:48:09 +0530

    Add random tasks data

---
+++ src/tasksData.js
@@ -0,0 +1,75 @@
+export const tasks = [
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+    {
+        name: 'Task_name',
+        lob: 'Medicaid',
+        subtasks: 2,
+        assigned: true,
+        assignedTo: 'Derek Roberts',
+        step: 'Review',
+        dueDate: new Date(2020, 10, 1 )
+    },
+
+]
